e75d9bf87c7259e937c23390a5f7515f4ddd5ffd
HowardHughes98 <82488909+HowardHughes98@users.noreply.github.com>
2021-05-08 03:32:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+for (let i = 0; i < 10; i++) {
+    // const element = array[index];
+    
+
+
+   var userInfo = {
+    userName: prompt('введите имя'),
+    userAge: prompt('введите возраст'),
+    
+    
+ 
+}
+    console.log(userInfo);
+
+    }
+    
+
+   
